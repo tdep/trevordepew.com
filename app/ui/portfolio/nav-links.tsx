c27ed0fd47bbb989a122979be55b8b7095a5937f
Trevor DePew <t.depewmusic@gmail.com>
2024-08-29 11:06:32 -0400

    moves home/logo to separate component, fixes dynamic bg color of nav links

--- app/ui/portfolio/nav-links.tsx
+++ app/ui/portfolio/nav-links.tsx
@@ -9,38 +9,66 @@ import {
 import Link from 'next/link';
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
+import TADIcon from "@/app/lib/TADIcon";
 
 // A Map of Links to display in the side navigation.
 const links = [
     { name: 'Home', href: '/', icon: HomeIcon },
     { name: 'Composition Portfolio', href: '/composition-portfolio', icon: MusicalNoteIcon },
     { name: 'Craft Portfolio', href: '/craft-portfolio', icon: PaintBrushIcon },
+    { name: 'Software Portfolio', href: '/software-portfolio', icon: CodeBracketIcon}
 ];
 
 export default function NavLinks() {
     const pathname = usePathname();
     return (
-        <> {/*Find a way to make these conditional based on current page */}
+        <>
             {links.map((link) => {
                 const LinkIcon = link.icon;
-                return (
-                    <Link
-                        key={link.name}
-                        href={link.href}
-                        className={clsx(
-                            "flex h-[48px] grow items-center justify-center gap-2 text-white-100 rounded-md bg-gray-400 p-3 text-xl font-medium hover:bg-amethyst-500 hover:text-white-100 md:flex-none md:justify-start md:p-2 md:px-3",
-                            {
-                                'bg-belize-500': link.name === 'Composition Portfolio',
-                                'bg-seagreen-500': link.name === 'Craft Portfolio',
-                                'bg-amethyst-500': pathname === link.href,
-                            },
-                        )}
-                    >
-                        <LinkIcon className={"w-6"} />
-                        <p className={"hidden md:block"}>{link.name}</p>
-                    </Link>
-                );
+                if (pathname !== link.href) { // omits the link of the page you're on
+                    return (
+                        <Link
+                            key={link.name}
+                            href={link.href}
+                            className={clsx(
+                                "flex h-[48px] grow items-center justify-center gap-2 text-white-100 rounded-md p-3 text-xl font-medium hover:bg-orange-500 hover:text-white-100 md:flex-none md:justify-start md:p-2 md:px-3",
+                                {
+                                    // dynamic background colors for side-nav links
+                                    'bg-belize-500': pathname === '/composition-portfolio',
+                                    'bg-seagreen-500': pathname === '/craft-portfolio',
+                                    'bg-amethyst-500': pathname === '/software-portfolio',
+                                },
+                            )}
+                        >
+                            <LinkIcon className={"w-6"}/>
+                            <p className={"hidden md:block"}>{link.name}</p>
+                        </Link>
+                    );
+                }
             })}
         </>
     );
+}
+
+export function HomeLogoNav() {
+    const pathname = usePathname();
+    return (
+        <>
+            {/* Home/Logo at top of nav */}
+            <Link
+                href={"/"}
+                className={clsx(
+                    "mb-2 flex h-20 items-center justify-center rounded-md p-4 md:h",
+                    {
+                        // dynamic background colors for side-nav home/logo link
+                        'bg-belize-500': pathname === '/composition-portfolio',
+                        'bg-seagreen-500': pathname === '/craft-portfolio',
+                        'bg-amethyst-500': pathname === '/software-portfolio',
+                    },
+                )}
+            >
+                <TADIcon width={120} height={120} />
+            </Link>
+        </>
+    )
 }
--- app/ui/sideNav.tsx
+++ app/ui/sideNav.tsx
@@ -1,19 +1,11 @@
-import Link from 'next/link';
-import NavLinks from '@/app/ui/portfolio/nav-links';
-import TadLogo from '@/app/ui/tad-logo';
+import NavLinks, {HomeLogoNav} from '@/app/ui/portfolio/nav-links';
 import { BackwardIcon } from "@heroicons/react/24/outline";
 import { goBack } from '@/app/lib/actions'
-import TADIcon from "@/app/lib/TADIcon";
 
 export default function SideNav() {
     return (
         <div className={"flex h-full flex-col px-3 py-4 md:px-2"}>
-            <Link
-                className={"mb-2 flex h-20 items-center justify-center rounded-md bg-seagreen-600 p-4 md:h"}
-                href={"/"}
-            >
-                <TADIcon width={120} height={120} />
-            </Link>
+            <HomeLogoNav />
             <div className={"flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2"}>
                 <NavLinks />
                 <div className={"hidden h-auto w-full grow rounded-md bg-gray-400 md:block"}>
@@ -23,6 +15,7 @@ export default function SideNav() {
                             await goBack();
                         }}
                     >
+                        {/* Back Button*/}
                         <button
                             className={"flex h-[48px] w=full grow items-center justify-center gap-2 rounded-md bg-gray-400 p-3 text-xl font-medium hover:bg-seagreen-600 hover:text-white-100 md:flex-none md:justify-start md:p-2 md:px-3"}
                         >
